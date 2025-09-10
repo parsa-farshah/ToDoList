83b77c44d7bdb519c2f6efd8b1ee8495d20b6c74
parsa <parsafarashah@gmail.com>
2025-09-10 16:39:40 +0330

    js: html: add complete and check box 2

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,54 @@ _inp.forEach((value, i) => {
 });
 
 // inp task
-let inpTask = document.querySelectorAll("#inpTask>input");
+let inpTaskUser = document.querySelector("#inpTask");
+let createTaskBtn = document.querySelector("#inpTask>button");
+let taskdiv = document.getElementById("taskAdd");
+let taskValue = "";
 
-let taskValue = inpTask.value;
+createTaskBtn.addEventListener("click", () => {
+  if (inpTaskUser.children[0].value === "") {
+    alert("enter task");
+  } else {
+    taskValue = inpTaskUser.children[0].value;
+    makeTask();
+  }
+
+  // reset
+  inpTaskUser.children[0].value = "";
+  inpTaskUser.children[0].focus();
+});
+
+function makeTask() {
+  taskdiv.innerHTML += `
+    <div  class=" task w-full mt-4 flex justify-between border-b border-b-gray-300">
+      <div class="w-full h-full flex items-center gap-2 pb-2">
+        <input
+        onclick="myTick(this)"
+         id="checkBox"
+          class="w-[20px] h-[20px] accent-[#c4c4c4ad]"
+          type="checkbox"
+        />
+        <h5>${taskValue}</h5>
+      </div>
+      <figure>
+        <img
+          class="w-[20px] h-[20px] cursor-pointer"
+          src="src/images/trash.png"
+          alt=""
+        />
+      </figure>
+    </div> `;
+}
+
+let completeDiv = document.querySelector("#complete");
+
+function myTick(s) {
+  let taskChange = s.closest(".task");
+  if (s.checked) {
+    completeDiv.appendChild(taskChange);
+    taskChange.classList.add("opacity-50");
+  } else {
+    taskChange.classList.add("opacity-100");
+  }
+}
